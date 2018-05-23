3597ca53e95a8d4cce214863bb9d4bf22c6b5fd4
林佳志乃 <hayashiyoshino@hayashikeishinonoMacBook-puro.local>
2018-05-23 17:02:44 +0900

    nameカラム、ビュー削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
 
   def show
     @nickname = current_user.nickname
-    @tweets = Tweet.where(user_id: current_user).order("created_at DESC").page(params[:page]).per(5)
+    @tweets = current_user.tweets.order("created_at DESC").page(params[:page]).per(5)
   end
 end
